7fe3641b70a2c16168537d3cf4f988ac82fc23f9
Piyumi Gunarathna <piyumi.gunarathna@gmail.com>
2023-01-11 10:48:56 +0800

    basic setup for app

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is a test', 'https://www.acouplecooks.com/wp-content/uploads/2020/01/Sheet-Pan-Dinner-019.jpg' ),
+    new Recipe('A test recipe 2', 'this is a test 2', 'https://cookieandkate.com/images/2021/09/baked-sweet-potato-recipe-1-1097x1650.jpg' )
+  ];
+  constructor() {}
 
+  ngOnInit() {}
 }
